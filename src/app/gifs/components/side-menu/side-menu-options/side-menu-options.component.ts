972ad94d83e8670238fc94d8e02563e507eb68b4
Luciano Figueroa <lumusitech@gmail.com>
2025-04-20 15:15:46 -0300

    feat: add menu options

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
   styles: ``,
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
