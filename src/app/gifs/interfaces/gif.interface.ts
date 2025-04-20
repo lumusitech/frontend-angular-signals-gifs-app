02ad603886a630487f3a1f2d704d1c87a4b35328
Luciano Figueroa <lumusitech@gmail.com>
2025-04-20 19:11:28 -0300

    feat: add fake gif interface and apply props drilling

---
+++ src/app/gifs/interfaces/gif.interface.ts
@@ -0,0 +1,5 @@
+export interface Gif {
+  id: string;
+  src: string;
+  alt: string;
+}
--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { GifListComponent } from '../../components/gif-list/gif-list.component';
+import { Gif } from '../../interfaces/gif.interface';
+
+const fakeGifs: Gif[] = [
+  {
+    id: '0',
+    src: 'image.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '1',
+    src: 'image-1.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '2',
+    src: 'image-2.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '3',
+    src: 'image-3.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '4',
+    src: 'image-4.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '5',
+    src: 'image.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '6',
+    src: 'image-6.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '7',
+    src: 'image-7.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '8',
+    src: 'image-8.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '9',
+    src: 'image-9.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '10',
+    src: 'image-10.jpg',
+    alt: 'test image',
+  },
+  {
+    id: '11',
+    src: 'image-11.jpg',
+    alt: 'test image',
+  },
+];
 
 @Component({
   selector: 'gifs-trending-page',
@@ -7,4 +71,6 @@ import { GifListComponent } from '../../components/gif-list/gif-list.component';
   templateUrl: './trending-page.component.html',
   styles: ``,
 })
-export default class TrendingPageComponent {}
+export default class TrendingPageComponent {
+  gifs = signal(fakeGifs);
+}
