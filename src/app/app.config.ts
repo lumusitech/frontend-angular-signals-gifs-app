e9162d861343d04b4350cb7128ae6cc4864e076b
Luciano Figueroa <lumusitech@gmail.com>
2025-04-26 00:35:20 -0300

    chore: add hash location strategy for manual deploy into netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
@@ -8,6 +9,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
     provideHttpClient(withFetch()),
   ],
 };
